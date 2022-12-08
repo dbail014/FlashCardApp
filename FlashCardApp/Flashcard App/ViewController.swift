f9d93b8febe2da891cbad08093f1c2356e37dfa1
Deshawn bailey <dbail014@odu.edu>
2022-12-08 06:03:05 -0500

    my first commit

---
+++ FlashCardApp/Flashcard App/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Flashcard App
+//
+//  Created by Deshawn Bailey on 2/15/20.
+//  Copyright © 2020 Deshawn Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var frontLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        
+        frontLabel.isHidden = true
+         
+    
+            
+                
+    }
+    
+}
+
